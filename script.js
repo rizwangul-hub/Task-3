2d2ecfb8640b1ec7de277881e8307cee44a7e6a2
rizwangul-hub <rizwangul-hub>
2026-02-06 09:09:15 +0500

    change in fil

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Task -1
 const user = {
  name: "Alex",
  address: {
@@ -14,22 +15,24 @@ let show2 =user?.number;
 let disply= user?.number || "not provided";
 console.log(disply);
 
-
+// Task-2
 let isLoggedIn = false;
 let show3 = isLoggedIn ? "Welcome back!" : "Please log in.";
 console.log(show3);
 
+// Bouns
 let isadmin = true;
 let show4 = isadmin ? "Welcome admin." : "welcome user.";
 console.log(show4);
 
+// Task-3
 let isPrice =null || undefined;;
 let show5 = isPrice ?? "Price not available.";
 console.log(show5); 
 
 
 
-
+// Task-5
 let hasMony = true;
 let monyWithPromise = new Promise((resolve, reject) => {
     if (hasMony) {
